3f23082a7af1fa7bb19c2cefa109cf8829d29f76
Juan Herrera <je.herrerap@uniandes.edu.co>
2024-11-01 15:46:52 -0500

    Se añade el menu de opciones

--- app/src/main/java/com/alphazetakapp/misoappvinilos/ui/album/list/AlbumListFragment.kt
+++ app/src/main/java/com/alphazetakapp/misoappvinilos/ui/album/list/AlbumListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.*
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -13,6 +14,9 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.alphazetakapp.misoappvinilos.R
 import com.alphazetakapp.misoappvinilos.databinding.FragmentAlbumListBinding
 import dagger.hilt.android.AndroidEntryPoint
+import androidx.core.view.MenuProvider
+import android.widget.Toast
+import androidx.lifecycle.Lifecycle
 
 @AndroidEntryPoint
 class AlbumListFragment : Fragment() {
--- app/src/main/java/com/alphazetakapp/misoappvinilos/ui/main/MainActivity.kt
+++ app/src/main/java/com/alphazetakapp/misoappvinilos/ui/main/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.alphazetakapp.misoappvinilos.ui.main
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
@@ -20,5 +25,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflar el archivo XML del menú
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.albums -> {
+                Toast.makeText(this, "Albums selected", Toast.LENGTH_SHORT).show()
+            }
+            R.id.artists -> {
+                Toast.makeText(this, "Artists selected", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
