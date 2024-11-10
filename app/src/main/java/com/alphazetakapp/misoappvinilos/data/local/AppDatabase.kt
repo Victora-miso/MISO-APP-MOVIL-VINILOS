7d77dddf209dd9b108b313da620a4b1f8b45803a
aira <a.solanop@uniandes.edu.co>
2024-11-10 05:12:08 -0500

    HU 06 Y 07 merge con Development

--- app/src/main/java/com/alphazetakapp/misoappvinilos/data/local/AppDatabase.kt
+++ app/src/main/java/com/alphazetakapp/misoappvinilos/data/local/AppDatabase.kt
@@ -25,12 +25,12 @@ abstract class AppDatabase : RoomDatabase() {
         val MIGRATION_2_3 = object : Migration(2, 3) {
             override fun migrate(db: SupportSQLiteDatabase) {
                 db.execSQL("CREATE TABLE IF NOT EXISTS `collectors` (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `email` TEXT NOT NULL, `name` TEXT NOT NULL, `telephone` TEXT NOT NULL)")
-                db.execSQL("INSERT INTO `collectors` (`id`, `email`, `name`, `telephone`) SELECT `id`, `email`, `name`, `telephone` FROM `collectors`")
-                db.execSQL("ALTER TABLE `collectors` RENAME TO `collector`")
+                //db.execSQL("INSERT INTO `collectors` (`id`, `email`, `name`, `telephone`) SELECT `id`, `email`, `name`, `telephone` FROM `collectors`")
+                //db.execSQL("ALTER TABLE `collectors` RENAME TO `collector`")
                 //db.execSQL("CREATE TABLE IF NOT EXISTS `musicians` (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `birthDate` TEXT NOT NULL, `description` TEXT NOT NULL, `image` TEXT NOT NULL, `name` TEXT NOT NULL, PRIMARY KEY(`id`))")
                 //db.execSQL("INSERT INTO `musicians` (`id`, `birthDate`, `description`, `image`, `name`) SELECT `id`, `birthDate`, `description`, `image`, `name` FROM `Musicians`")
                 //db.execSQL("ALTER TABLE `musicians` RENAME TO `Musicians`")
-                db.execSQL("CREATE TABLE IF NOT EXISTS `album` (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,`name` TEXT NOT NULL,`cover` TEXT NOT NULL, `releaseDate` TEXT NOT NULL,`description` TEXT NOT NULL,`genre` TEXT NOT NULL,`recordLabel` TEXT NOT NULL)")
+                //db.execSQL("CREATE TABLE IF NOT EXISTS `album` (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,`name` TEXT NOT NULL,`cover` TEXT NOT NULL, `releaseDate` TEXT NOT NULL,`description` TEXT NOT NULL,`genre` TEXT NOT NULL,`recordLabel` TEXT NOT NULL)")
 
             }
         }
