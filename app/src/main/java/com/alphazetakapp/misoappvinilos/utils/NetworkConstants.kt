0359e593c8b3bbfd63db22f1808e9478dc856a00
Victora-miso <v.duarteq@uniandes.edu.co>
2024-11-24 23:24:58 -0500

    Inspection code fixed.

--- app/src/main/java/com/alphazetakapp/misoappvinilos/utils/NetworkConstants.kt
+++ app/src/main/java/com/alphazetakapp/misoappvinilos/utils/NetworkConstants.kt
@@ -2,5 +2,5 @@ package com.alphazetakapp.misoappvinilos.utils
 
 object NetworkConstants {
     //const val BASE_URL = "http://10.0.2.2:3000/" //Por ejemplo: "https://api.vinilos.com/
-    const val BASE_URL = "https://vynils-back-heroku.herokuapp.com/" //Por ejemplo: "https://api.vinilos.com/
+    const val BASE_URL = "https://backvynils-q6yc.onrender.com/" //Por ejemplo: "https://api.vinilos.com/
 }
